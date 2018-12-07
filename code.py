cad0c1d2184e1a99f590824d645b579641e25d1c
Boris Reiss <reissb@email.arizona.edu>
2018-12-07 10:40:59 -0700

    add all files

---
+++ code.py
@@ -0,0 +1,4 @@
+IMPORTANT: Git will only track files in the folder where the .git repository is and and files 
+inside of that folder. So if your project is not contained under one folder, you should move all of 
+the files you need to track to go under one folder or you will need to have multiple Git 
+repositories.
